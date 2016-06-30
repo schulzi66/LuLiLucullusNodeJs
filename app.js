d88fa522d254458ffdc8330936f0af314b52598f
schulzi66 <marius.schulze@t-online.de>
2016-06-30 16:11:29 +0200

    clean up

--- app.js
+++ app.js
@@ -9,28 +9,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// Facebook Login
-//https://github.com/passport/express-4.x-facebook-example/blob/master/server.js
-// var passport = require('passport');
-// var Strategy = require('passport-facebook').Strategy;
-//
-// passport.use(new Strategy({
-//     clientID: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: 'http://localhost:3000/login/facebook/return'
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     return cb(null, profile);
-// }));
-//
-// passport.serializeUser(function(user, cb) {
-//   cb(null, user);
-// });
-//
-// passport.deserializeUser(function(obj, cb) {
-//   cb(null, obj);
-// });
-
 // setup routes
 var index = require('./routes/index');
 var contact = require('./routes/contact');
@@ -51,18 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// // Use application-level middleware for common functionality, including
-// // logging, parsing, and session handling.
-// app.use(require('morgan')('combined'));
-// app.use(require('cookie-parser')());
-// app.use(require('body-parser').urlencoded({ extended: true }));
-// app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-//
-// // Initialize Passport and restore authentication state, if any, from the
-// // session.
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 
 // use routes
@@ -102,66 +68,4 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-/**
- *
-
-
-passport.use(new LocalStrategy(function(username, password,done){
-    Users.findOne({ username : username},function(err,user){
-        if(err) { return done(err); }
-        if(!user){
-            return done(null, false, { message: 'Incorrect username.' });
-        }
-
-        hash( password, user.salt, function (err, hash) {
-            if (err) { return done(err); }
-            if (hash == user.hash) return done(null, user);
-            done(null, false, { message: 'Incorrect password.' });
-        });
-    });
-}));
-
-passport.use(new FacebookStrategy({
-        clientID: "YOUR ID",
-        clientSecret: "YOUR CODE",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        FbUsers.findOne({fbId : profile.id}, function(err, oldUser){
-            if(oldUser){
-                done(null,oldUser);
-            }else{
-                var newUser = new FbUsers({
-                    fbId : profile.id ,
-                    email : profile.emails[0].value,
-                    name : profile.displayName
-                }).save(function(err,newUser){
-                    if(err) throw err;
-                    done(null, newUser);
-                });
-            }
-        });
-    }
-));
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-
-passport.deserializeUser(function(id, done) {
-    FbUsers.findById(id,function(err,user){
-        if(err) done(err);
-        if(user){
-            done(null,user);
-        }else{
-            Users.findById(id, function(err,user){
-                if(err) done(err);
-                done(null,user);
-            });
-        }
-    });
-});
- */
 module.exports = app;
--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-app.get("/auth/facebook", passport.authenticate("facebook",{ scope : "email"}));
-
-app.get("/auth/facebook/callback",
-    passport.authenticate("facebook",{ failureRedirect: '/login'}),
-    function(req,res){
-        res.render("loggedin", {user : req.user});
-    }
-); 
