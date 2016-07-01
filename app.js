b9786fc27c2fe0cf2464841d925743e0f4f0d79f
schulzi66 <marius.schulze@t-online.de>
2016-07-01 10:52:09 +0200

    user sessions merge conflicts

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: conf.session.secret}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
