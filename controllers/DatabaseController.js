e28933b3100dbfa66784ac271fa4f1daa11ac95d
Julian Ulonska <Julian.Ulonska@pirobase.de>
2016-07-04 18:53:27 +0200

    database connection, new directory for sql scripts

--- controllers/DatabaseController.js
+++ controllers/DatabaseController.js
@@ -4,25 +4,33 @@
  */
 
 var mysql = require('mysql');
-var DatabaseController = function() {
+var DatabaseController = function () {
 }
 
 DatabaseController.prototype.connect = function (user, password, dbo, startServerCallback) {
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : user,
-    password : password,
-    database : dbo
-  });
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: user,
+        password: password,
+        database: dbo
+    });
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-    startServerCallback();
-  });
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected as id ' + connection.threadId);
+        startServerCallback();
+    });
+
+    connection.query('CREATE TABLE IF NOT EXISTS test_table (id int not NULL)', function (err, rows, fields) {
+        if (!err) {
+            console.log("Success " + rows)
+        } else {
+            console.log("Error while proceeding");
+        }
+    });
 }
 
 module.exports = DatabaseController;
