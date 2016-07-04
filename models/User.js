d9df7b04de4c829798f33dda176332e6a4c86ce7
Julian Ulonska <Julian.Ulonska@pirobase.de>
2016-07-04 22:14:10 +0200

    initial signup, recipes style changes, user model prototype

--- models/User.js
+++ models/User.js
@@ -1,6 +1,47 @@
 var DbController = require('./../controllers/DatabaseController');
-var bcrypt   = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+
 // req.app.get('connection').usercollection.find();
 
 var _dbController = new DbController();
 var connection = _dbController.connect(startServer);
+
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+
+    local: {
+        email: String,
+        password: String,
+    },
+    facebook: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+    twitter: {
+        id: String,
+        token: String,
+        displayName: String,
+        username: String
+    },
+    google: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    }
+
+});
+
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
