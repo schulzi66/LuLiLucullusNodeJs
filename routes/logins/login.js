88b5091a033f80dd333d5cbd2b2dc1c5af6a4bdd
schulzi66 <marius.schulze@t-online.de>
2016-07-04 11:10:47 +0200

    fixed logout redircect

--- routes/logins/login.js
+++ routes/logins/login.js
@@ -1,19 +1,11 @@
 var conf = require('../../conf.json');
 var express = require('express');
 var router = express.Router();
-//
-// var googleLogin = require('./google');
-// router.use('/google', googleLogin);
-//
-// var facebookLogin = require('./facebook');
-// router.use('/login/facebook', facebookLogin);
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login');
 });
-
 
 router.get('/profile',
   function(req, res){
