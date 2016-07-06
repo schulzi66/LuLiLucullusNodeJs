913661ebbbed5907ff54dc4c38f56fde95da40e8
Julian Ulonska <Julian.Ulonska@pirobase.de>
2016-07-06 13:44:07 +0200

    revert changes

--- routes/logins/signup.js
+++ routes/logins/signup.js
@@ -3,7 +3,7 @@
 // load all the things we need
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
 var conf = require('../../conf.json');
 
 var mysql = require('mysql');
@@ -17,7 +17,18 @@ var connection = mysql.createConnection({
     charset: conf.database.charset
 });
 
-router.get('/', function(req, res) {
+/* GET signup page. */
+router.get('/', function (req, res, next) {
+    res.render('signup', {user: req.session.user});
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
+
+router.get('/', function (req, res) {
     res.render('signup');
 });
 
