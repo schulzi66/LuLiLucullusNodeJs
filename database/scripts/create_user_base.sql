43f2e4b31852c38b4ce517c5d60b721a0c84f26f
Julian <julian.christer@web.de>
2016-07-07 15:05:47 +0200

    changed creation script to new user table

--- database/scripts/create_user_base.sql
+++ database/scripts/create_user_base.sql
@@ -1,11 +1,11 @@
 INSERT INTO USER
 VALUES ('Mustermann', 'Max', 'max@mustermann.de', 'password', 'Musterstraße', 'Musterort', 12345, 'Musterstraße',
-           'Musterort', 12345);
+           'Musterort', 12345, true);
 
 INSERT INTO USER
 VALUES ('Musterfrau', 'Marta', 'marta@musterfrau.de', 'password', 'Musterstraße', 'Musterort', 12345, 'Musterstraße',
-           'Musterort', 12345);
+           'Musterort', 12345, true);
 
 INSERT INTO USER
 VALUES ('Mustermann', 'Mario', 'mario@mustermann.de', 'password', 'Musterstraße', 'Musterort', 12345, 'Musterstraße',
-           'Musterort', 12345);
+           'Musterort', 12345, true);
