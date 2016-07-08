64d37ac529031e84f693110eeb29e3f62d56006d
Julian <julian.christer@web.de>
2016-07-08 21:37:20 +0200

    code formatting, style for message alert

--- routes/logins/signup.js
+++ routes/logins/signup.js
@@ -7,10 +7,10 @@ var DbController = require('../../controllers/DatabaseController');
 router.get('/', function (req, res, next) {
     res.render('signup', {user: req.session.user, message: req.session.message});
     req.session.message = undefined;
-console.log("req.session.message: " + req.session.message);
+    console.log("req.session.message: " + req.session.message);
 });
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
     var _dbController = new DbController();
     _dbController.signup(req, res, true);
 });
