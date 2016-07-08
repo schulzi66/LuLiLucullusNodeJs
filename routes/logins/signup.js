c926d8b2357f6a941cf48c3c2bb8eca0dfe33ca1
schulzi66 <marius.schulze@t-online.de>
2016-07-08 18:32:42 +0200

    forgott a file

--- routes/logins/signup.js
+++ routes/logins/signup.js
@@ -5,12 +5,14 @@ var DbController = require('../../controllers/DatabaseController');
 
 /* GET signup page. */
 router.get('/', function (req, res, next) {
-    res.render('signup', {user: req.session.user});
+    res.render('signup', {user: req.session.user, message: req.session.message});
+    req.session.message = undefined;
+console.log("req.session.message: " + req.session.message);
 });
 
 router.post('/', function(req, res){
     var _dbController = new DbController();
-      _dbController.signup(req, res, true);
+    _dbController.signup(req, res, true);
 });
 
 module.exports = router;
