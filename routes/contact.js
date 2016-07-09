43f83d3e243f6cdd2180486af4e0013e0b58be0b
Julian <julian.christer@web.de>
2016-07-09 16:13:21 +0200

    sending email after submitting contact formular

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
+var conf = require('../conf.json');
+var nodemailer = require('nodemailer');
 /* GET contact page. */
-router.get('/', function(req, res, next) {
-  res.render('contact', { user: req.session.user});
+router.get('/', function (req, res, next) {
+    res.render('contact', {user: req.session.user});
+});
+
+router.post('/', function (req, res) {
+    var transporter = nodemailer.createTransport({
+        service: conf.mail.service,
+        auth: {
+            user: conf.auth.mail.user, // Your email id
+            pass: conf.auth.mail.password // Your password
+        }
+    });
+
+    var mailOptions = {
+        from: req.body.email, // sender address
+        to: 'ulonska.julian@gmail.com', // list of receivers
+        subject: 'Kontakt Formular', // Subject line
+        text: req.body.contact_message//, // plaintext body
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            req.session.message = "Ihre Anfrage wurde erfolgreich &uuml;bermittelt.";
+            res.redirect('/contact');
+        }
+    });
 });
 
 module.exports = router;
