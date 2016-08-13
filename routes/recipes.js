49a3706f68f6f0d854c68b080fe926735d1820c2
Julian <julian.christer@web.de>
2016-08-13 13:40:02 +0200

    adjustment for recipe detail page, protect recipes page with login

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET recipes page. */
 router.get('/', function(req, res) {
-  
+    if(req.session.user) {
     res.render('recipes', { user: req.session.user} );
+    } else {
+        res.render('login', { user: req.session.user} );
+    }
 });
 
 module.exports = router;
