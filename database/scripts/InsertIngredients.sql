3c56e17b7c4cec1e9a479aaa0480a7944a01fe1f
schulzi66 <marius.schulze@t-online.de>
2016-08-16 10:03:05 +0200

    fixed ingredients sql script

--- database/scripts/InsertIngredients.sql
+++ database/scripts/InsertIngredients.sql
@@ -1,30 +1,65 @@
-insert into recipeingredients set
-amount="50",
-unitid="0",
-ingredientsid="0",
-ingredientname="Zutat1";
+insert into ingredients set
+   ingredientID = 0
+  ,ingredientName = "Butter";
 
-insert into recipeingredients set
-amount="500",
-unitid="2",
-ingredientsid="1",
-ingredientname="Zutat2";
+insert into ingredients set
+   ingredientID = 1
+  ,ingredientName = "Mehl";
 
-insert into recipeingredients set
-amount="5",
-unitid="1",
-ingredientsid="2",
-ingredientname="Zutat3";
+  insert into ingredients set
+   ingredientID = 2
+  ,ingredientName = "Milch";
 
-insert into recipeingredients set
-amount="1",
-unitid="3",
-ingredientsid="3",
-ingredientname="Zutat4";
+  insert into ingredients set
+   ingredientID = 3
+  ,ingredientName = "Eier";
 
-insert into recipeingredients set
-amount="50",
-unitid="0",
-ingredientsid="4",
-ingredientname="Zutat5";
+  INSERT INTO RecipeIngredients SET
+    amount = '10',
+    recipeID = '0',
+    ingredientID = '0',
+    unitID = '0';
 
+
+INSERT INTO RecipeIngredients SET
+  amount = '10',
+  recipeID = '1',
+  ingredientID = '0',
+  unitID = '0';
+
+INSERT INTO RecipeIngredients SET
+  amount = '100',
+  recipeID = '1',
+  ingredientID = '1',
+  unitID = '2';
+
+INSERT INTO RecipeIngredients SET
+  amount = '500',
+  recipeID = '1',
+  ingredientID = '2',
+  unitID = '3';
+
+
+INSERT INTO RecipeIngredients SET
+  amount = '1',
+  recipeID = '2',
+  ingredientID = '1',
+  unitID = '1';
+
+INSERT INTO RecipeIngredients SET
+  amount = '1',
+  recipeID = '2',
+  ingredientID = '3',
+  unitID = '3';
+
+INSERT INTO RecipeIngredients SET
+  amount = '150',
+  recipeID = '2',
+  ingredientID = '2',
+  unitID = '2';
+
+INSERT INTO RecipeIngredients SET
+  amount = '40',
+  recipeID = '2',
+  ingredientID = '4',
+  unitID = '1';
