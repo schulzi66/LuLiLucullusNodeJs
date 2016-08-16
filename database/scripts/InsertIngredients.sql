d05a9e5fcc95cd6bccca9228f64d384bc3f95faf
Julian Ulonska <Ulonska.julian@gmail.com>
2016-08-16 09:35:34 +0200

    Initial Script for Ingredients

---
+++ database/scripts/InsertIngredients.sql
@@ -0,0 +1,20 @@
+insert inti recipeingredients set
+ingredientsid="0",
+ingredientname="Zutat1";
+
+insert inti recipeingredients set
+ingredientsid="1",
+ingredientname="Zutat2";
+
+insert inti recipeingredients set
+ingredientsid="2",
+ingredientname="Zutat3";
+
+insert inti recipeingredients set
+ingredientsid="3",
+ingredientname="Zutat4";
+
+insert inti recipeingredients set
+ingredientsid="4",
+ingredientname="Zutat5";
+
