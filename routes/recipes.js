be5b12a2147bece7923228cd98ae23503f098d00
schulzi66 <marius.schulze@t-online.de>
2016-08-18 12:12:24 +0200

    secured privat url and cleand up admin login page

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET recipes page. */
 router.get('/', function(req, res) {
+  if(req.session.user !== undefined) {
     res.render('recipes', { user: req.session.user} );
+  } else {
+    res.render('login' );
+  }
 });
 
 module.exports = router;
