8e800f9f557dba7dd61c3bc1ebdf0d451ea4efbb
Julian <julian.christer@web.de>
2016-08-25 14:30:36 +0200

    refactoring: moved method to util.js

--- public/dev-js/pictureRef.js
+++
@@ -1,8 +0,0 @@
-function convertPictureRefToPath(pictureRef) {
-    var imgDir = "img/";
-    var fileExtension = [".png", ".jpg"];
-    pictureRef = pictureRef.split('pictureRef')[1].toLowerCase();
-    var path = imgDir + pictureRef + fileExtension[0];
-    //TODO: check if file exists
-    return path;
-}
--- public/dev-js/util.js
+++ public/dev-js/util.js
@@ -1,10 +1,6 @@
 function Util() {
 }
 
-Util.lookup = function (fileExtension) {
-    console.log(fileExtension);
-}
-
 Util.firstLetterToUpperCase = function (str) {
     return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -13,7 +9,7 @@ Util.firstLetterToUpperCase = function (str) {
 
 Util.fixRootEntry = function (str) {
     console.log(str);
-    if(str === 'Home') {
+    if (str === 'Home') {
         str = "#";
         return str;
     } else {
@@ -25,15 +21,24 @@ Util.lookup = function (fileExtension) {
     return fileExtension;
 }
 
-Util.convertMySQLTimestampToValidTimestamp = function(timestamp) {
+Util.convertMySQLTimestampToValidTimestamp = function (timestamp) {
     moment.locale('de');
-    return moment(timestamp*1000).format('LL');
+    return moment(timestamp * 1000).format('LL');
 }
 
 Util.getMaturityPeriod = function (orderDate, maturityDate) {
-    return (Math.abs(maturityDate - orderDate)/60/60/24);
+    return (Math.abs(maturityDate - orderDate) / 60 / 60 / 24);
 }
 
 Util.calculateIngredientsAmount = function () {
     console.log("test");
+}
+
+Util.convertPictureRefToPath = function (pictureRef) {
+    var imgDir = "img/";
+    var fileExtension = [".png", ".jpg"];
+    pictureRef = pictureRef.split('pictureRef')[1].toLowerCase();
+    var path = imgDir + pictureRef + fileExtension[0];
+    //TODO: check if file exist
+    return path;
 }
