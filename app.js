ddc2d9c50ebedae7e839c943ea58d927366d1613
Julian <julian.christer@web.de>
2016-08-25 15:27:50 +0200

    revert changes due to a :bug:

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cookieParser());
