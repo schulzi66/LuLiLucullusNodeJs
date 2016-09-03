082fab4a1fea9ec8a951060ce8cb19d3b0d7b861
Julian <julian.christer@web.de>
2016-09-03 15:16:05 +0200

    typo..

--- public/thirdparty/dev-js/bootstrap-datepicker.js
+++ public/thirdparty/dev-js/bootstrap-datepicker.js
@@ -1718,7 +1718,7 @@
 		en: {
 			days: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
 			daysShort: ["Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
-			daysMin: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "so"],
+			daysMin: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
 			months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
 			monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
 			today: "Heute",
