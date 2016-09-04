b36f4a049d2bd14cf36c5908712560ea3535aecd
Julian <julian.christer@web.de>
2016-09-04 14:34:13 +0200

    upload form

--- routes/admin/upload.js
+++ routes/admin/upload.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var DatabaseController = require('../../controllers/DatabaseController');
 var _dbController = new DatabaseController();
 
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
 /* GET upload page. */
 router.get('/', function (req, res) {
     if (req.session.user !== undefined && req.session.user.isAdmin !== undefined) {
@@ -13,7 +17,30 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-    console.log(req.body);
+    /*// create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req)*/
     _dbController.uploadRecipe(req.body, function () {
         console.log("I DID IT");
     });
