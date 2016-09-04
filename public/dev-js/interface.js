762ebce3a32f4b71b5860ea1c471313131469334
colfin_96 <fing.co2009@gmail.com>
2016-09-04 21:49:31 +0200

    AUTOCOMPLETE :smile:

--- public/dev-js/interface.js
+++ public/dev-js/interface.js
@@ -224,10 +224,15 @@ function loadFilterOptions() {
             '<div class="panel panel-default" id="filterOptionsName" class="col-md-4">' +
             '<div class="panel-heading">Name</div>' +
             '<div class="ui-widget">' +
-            '<input id="filterOptionTextInput" type="text" class="form-control" name="recipeNames" data-type="recipeName" />' +
+            '<input id="filterOptionTextInput" class="ui-autocomplete-input" autocomplete="off" name="recipeNames" data-type="recipeName">' +
+            //'<input id="filterOptionTextInput" type="text" class="form-control" name="recipeNames" data-type="recipeName" />' +
             '</div>' +
             '</div>'
         );
+
+        $("#filterOptionTextInput").autocomplete({
+            source: recipeNames
+        });
     });
 }
 
@@ -285,35 +290,6 @@ $(document).ready(function () {
         }
         console.log(filterOptions);
         loadFilteredRecipes(filterOptions);
-    });
-    $(function() {
-        var availableTags = [
-             "ActionScript",
-             "AppleScript",
-             "Asp",
-             "BASIC",
-             "C",
-             "C++",
-             "Clojure",
-             "COBOL",
-             "ColdFusion",
-             "Erlang",
-             "Fortran",
-             "Groovy",
-             "Haskell",
-             "Java",
-             "JavaScript",
-             "Lisp",
-             "Perl",
-             "PHP",
-             "Python",
-             "Ruby",
-             "Scala",
-             "Scheme"
-        ];
-        $("#filterOptionTextInput").autocomplete({
-            source: availableTags
-        });
     });
 
     /**
