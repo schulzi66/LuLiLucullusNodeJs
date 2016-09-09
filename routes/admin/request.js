7057b581ac5a9bd9d01d7136ae1fb764583417a1
Julian Ulonska <Julian.Ulonska@pirobase.de>
2016-09-09 09:56:46 +0200

    form for request overview, fixed route

--- routes/admin/request.js
+++ routes/admin/request.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET orders page. */
 router.get('/', function (req, res) {
     if (req.session.user !== undefined && req.session.user.isAdmin !== undefined) {
-        res.render('orders', {user: req.session.user});
+        res.render('request', {user: req.session.user});
     } else {
         res.render('administration-login');
     }
